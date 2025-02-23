1c8fcdda5cfe801fb20caf681b0c4c245a92be16
Valtteri Kuosmanen <valtterikuo@gmail.com>
2025-02-23 13:32:34 +0200

    Create option to add rat pups

--- rat_entity.cpp
+++ rat_entity.cpp
@@ -7,6 +7,19 @@ RatEntity::RatEntity(std::string inputGender, std::string inputName, std::string
     age = 0;
 }
 
+void RatEntity::addPups(std::shared_ptr<RatEntity> pup) {
+    pups.push_back(pup);
+    auto self = shared_from_this();
+    if (self) {
+        if (RatEntity::gender == "Male") {
+            pup->father = self;
+        }
+        if (RatEntity::gender == "Female") {
+            pup->mother = self;
+        }
+    }
+}
+
 std::string RatEntity::getRatGender() {
     return RatEntity::gender;
 }
@@ -17,4 +30,12 @@ std::string RatEntity::getRatName() {
 
 std::string RatEntity::getRatDynasty() {
     return RatEntity::dynastyName;
+}
+
+std::string RatEntity::getRatFatherName() {
+    return RatEntity::father;
+}
+
+std::string RatEntity::getRatMotherName() {
+    return RatEntity::mother;
 }
--- rat_entity.h
+++ rat_entity.h
@@ -1,18 +1,28 @@
 #ifndef RATENTITY_H
 #define RATENTITY_H
 
+#include <iostream>
+#include <vector>
+#include <memory>
 #include <string>
 
-class RatEntity {
+// Inherit from std::enable_shared_from_this<RatEntity>
+class RatEntity : public std::enable_shared_from_this<RatEntity> {
+    int age;
     std::string gender;
+    std::vector<std::shared_ptr<RatEntity>> pups;
+    std::weak_ptr<RatEntity> father;
+    std::weak_ptr<RatEntity> mother;
     std::string givenName;
     std::string dynastyName;
-    int age;
   public:
     RatEntity(std::string inputGender, std::string inputName, std::string inputDynasty);
+    void addPups(std::shared_ptr<RatEntity>);
     std::string getRatGender();
     std::string getRatName();
     std::string getRatDynasty();
+    std::string getRatFatherName();
+    std::string getRatMotherName();
 };
 
 #endif
